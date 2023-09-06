0ff0d2a0b078beb7d62efe32fb9778cf144f669b
Bianca Nenciu <nbianca@users.noreply.github.com>
2023-09-06 14:30:02 +0300

    DEV: Add test to make sure footer is rendered (#32)

---
+++ test/acceptance/easy-footer-test.js
@@ -0,0 +1,19 @@
+import { visit } from "@ember/test-helpers";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
+import { test } from "qunit";
+
+acceptance("easy-footer", function (needs) {
+  needs.user();
+
+  test("shows the footer", async function (assert) {
+    await visit("/categories");
+
+    assert.dom(".below-footer-outlet").exists();
+    assert
+      .dom(".below-footer-outlet .first-box .heading")
+      .hasText("This is a header");
+    assert.dom(".below-footer-outlet .second-box .links").exists();
+    assert.dom(".below-footer-outlet .third-box .footer-links").exists();
+    assert.dom(".below-footer-outlet .third-box .social").exists();
+  });
+});
