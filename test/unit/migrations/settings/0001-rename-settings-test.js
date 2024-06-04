813c832dbf69ecd26f4f691e077932aef8d228f6
Jarek Radosz <jradosz@gmail.com>
2024-06-04 21:06:52 +0200

    DEV: Add theme name prefix to test names

--- test/unit/migrations/settings/0001-rename-settings-test.js
+++ test/unit/migrations/settings/0001-rename-settings-test.js
@@ -1,37 +1,40 @@
 import { module, test } from "qunit";
 import migrate from "../../../../migrations/settings/0001-rename-settings";
 
-module("Unit | Migrations | Settings | 0001-rename-settings", function () {
-  test("migrate", function (assert) {
-    const settings = new Map(
-      Object.entries({
-        Heading: "some header",
-        Blurb: "some blurb",
-        Link_sections: "section1|section2",
-        Links: "some_links",
-        Small_links: "some,small,links",
-        Social_links: "some,social,links",
-        Show_footer_on_login_required_page: true,
-      })
-    );
+module(
+  "Easy Footer | Unit | Migrations | Settings | 0001-rename-settings",
+  function () {
+    test("migrate", function (assert) {
+      const settings = new Map(
+        Object.entries({
+          Heading: "some header",
+          Blurb: "some blurb",
+          Link_sections: "section1|section2",
+          Links: "some_links",
+          Small_links: "some,small,links",
+          Social_links: "some,social,links",
+          Show_footer_on_login_required_page: true,
+        })
+      );
 
-    const result = migrate(settings);
+      const result = migrate(settings);
 
-    assert.deepEqual(
-      Array.from(result),
-      Array.from(
-        new Map(
-          Object.entries({
-            heading: "some header",
-            blurb: "some blurb",
-            link_sections: "section1|section2",
-            links: "some_links",
-            small_links: "some,small,links",
-            social_links: "some,social,links",
-            show_footer_on_login_required_page: true,
-          })
+      assert.deepEqual(
+        Array.from(result),
+        Array.from(
+          new Map(
+            Object.entries({
+              heading: "some header",
+              blurb: "some blurb",
+              link_sections: "section1|section2",
+              links: "some_links",
+              small_links: "some,small,links",
+              social_links: "some,social,links",
+              show_footer_on_login_required_page: true,
+            })
+          )
         )
-      )
-    );
-  });
-});
+      );
+    });
+  }
+);
